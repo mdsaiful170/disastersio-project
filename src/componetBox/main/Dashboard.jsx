07908116580e3931f1e0deec4a55f5e273e5327b
mdsaiful170 <saifulsylhet00@gmail.com>
2024-10-19 02:33:59 +0600

    path erro solve

--- src/componetBox/main/Dashboard.jsx
+++ src/componetBox/main/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Container } from "../shearComponet/Container";
+import { Container } from "../shearComponet/Container.jsx";
 import { carddatabox } from "../../../lib/databox";
 import { MsgBox } from "../shearComponet/MsgBox";
 import { Searchbox } from "../shearComponet/Searchbox";
